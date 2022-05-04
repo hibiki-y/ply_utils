f2ca6dc6237f59c735819f661a6023588ab38d6f
hibiki-y <yamahibi.0309@gmail.com>
2022-05-04 13:18:32 +0900

    Add: add_cutter

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,8 +1,11 @@
+extern crate cutter;
 extern crate decoder;
 fn main() {
     println!("cut start");
+    cutter::cut_property("../test/test.ply", "../out/output_test-1.ply", 0, 3);
+    println!("decode start");
     decoder::to_string(
-        "./original/dancer_vox11_00000001.ply",
-        "./out/decode_test11.ply",
+        "../original/dancer_vox11_00000001.ply",
+        "../out/decode_test11.ply",
     );
 }
